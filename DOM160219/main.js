44ba251cc91cc0c21dec13e8773b6d7855adf034
tohid <tohidfla@gmail.com>
2019-02-16 10:24:27 +0100

    lets push on to github for first time

--- DOM160219/main.js
+++ DOM160219/main.js
@@ -1,8 +1,16 @@
 let textAreaElement = document.getElementsByName("textArea");
 textAreaElement.style.backgroundColor = "blue";
 
-let bodyElement = document.getElementsByName("body");
+let bodyElement = document.body;
 
 let inputElementByJS = document.createElement("input");
-inputElementByJS.placeholder = "inputElementByJS";
-bodyElement.appendChild("inputElementByJS");
+inputElementByJS.value = "inputElementByJS";
+bodyElement.appendChild(inputElementByJS);
+
+let inputElementsPackage = document.getElementsByTagName("input");
+
+for(let i = 0; i<inputElementsPackage.length ; i++){
+    //inputElementsPackage[i].value = "input element "+ (i+1);
+    inputElementsPackage[i].style.fontSize = 12+i ;
+    inputElementsPackage[i].placeholder = "I am placeholder :) " +i;
+}
