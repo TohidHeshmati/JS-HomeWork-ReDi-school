4c443e7d808e9b7218a47907e720928cfb0121e9
tohid <tohidfla@gmail.com>
2019-02-16 10:06:05 +0100

    i am doing something wrong

---
+++ DOM160219/main.js
@@ -0,0 +1,8 @@
+let textAreaElement = document.getElementsByName("textArea");
+textAreaElement.style.backgroundColor = "blue";
+
+let bodyElement = document.getElementsByName("body");
+
+let inputElementByJS = document.createElement("input");
+inputElementByJS.placeholder = "inputElementByJS";
+bodyElement.appendChild("inputElementByJS");
