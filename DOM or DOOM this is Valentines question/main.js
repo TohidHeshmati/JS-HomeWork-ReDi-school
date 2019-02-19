077ca7621a867c3422d37019560db48c7227c823
tohid <tohidfla@gmail.com>
2019-02-19 16:12:11 +0100

    for Each with two dimentional array

--- DOM or DOOM this is Valentines question/main.js
+++ DOM or DOOM this is Valentines question/main.js
@@ -1,6 +1,9 @@
-let booksArray = ['Anna Karenina', 'Madame Bovary', 'War and Peace'];
+let booksArray = [['Anna Karenina','Leo Tolstoy'], ['Madame Bovary', 'Gustave Flaubert'],['War and Peace', 'Leo Tolstoy'] ];
 console.log(booksArray[0]);
 
-booksArray.forEach(function (bookname, inPositionNumber){
-    console.log("the book Name is: "+bookname, " and it is in Position: "+inPositionNumber);
+booksArray.forEach(function (bookName, index){
+    // i assume that it returns an array so i will go for first as book name and second as author
+
+    console.log("the book Name is: "+bookName[0]+" and the author is: "+bookName[1]+ " at shelf: "+index);
 });
+
